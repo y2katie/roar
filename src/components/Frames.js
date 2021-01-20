d2b74587db1c15f89829babc61317bbab6644a95
katyvonkaty <kj0203@gmail.com>
2021-01-19 19:23:04 -0500

    updated

--- src/components/Frames.js
+++ src/components/Frames.js
@@ -3,21 +3,40 @@ import { render } from "react-dom";
 import ParticlesBg from "particles-bg";
 
 
-class Frames extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "React"
-    };
-  }
+const Frames = () => {
 
-  render() {
+  let config = {
+      num: [1, 7],
+      rps: 0.1,
+      radius: [5, 40],
+      life: [1.5, 3],
+      v: [2, 3],
+      tha: [-40, 40],
+      // body: "./img/icon.png", // Whether to render pictures
+      // rotate: [0, 20],
+      alpha: [0.6, 0],
+      scale: [1, 0.1],
+      position: "center", // all or center or {x:1,y:1,width:100,height:100}
+      color: ["random", "#ff0000"],
+      cross: "dead", // cross or bround
+      random: 15,  // or null,
+      g: 5,    // gravity
+      // f: [2, -1], // force
+      onParticleUpdate: (ctx, particle) => {
+          ctx.beginPath();
+          ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
+          ctx.fillStyle = particle.color;
+          ctx.fill();
+          ctx.closePath();
+      }
+    }
+
     return (
       <div>
-        <ParticlesBg type="random" bg={true}/>
+        <ParticlesBg config={config} type="circle" num={20} bg={true} className="testing"/>
       </div>
     );
-  }
+
 }
 
 export default Frames
