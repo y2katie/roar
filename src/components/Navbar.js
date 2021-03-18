09f9e25d555bf560a147401e80af097f6fda73dc
katie <kj0203@gmail.com>
2021-03-18 11:37:45 -0400

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,40 @@
-import React from 'react'
-import {
-  Icon
-} from "semantic-ui-react";
-import { Link }  from "react-router-dom"
+import React from "react";
+import { Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div class="ui secondary menu topMenu stackable">
-    <Link to= "/" class='item'>
-       <h2 style={{fontWeight:"700"}}> <Icon name="pin" />TickerTok </h2>
-    </Link>
+      <Link to="/" class="item">
+        <h2 style={{ fontWeight: "700" }}>
+          {" "}
+          <Icon name="pin" />
+          TickerTok{" "}
+        </h2>
+      </Link>
 
-    <div class="right menu">
-    <Link to="/carousel" class="item">
-      Press
-    </Link>
-    <Link to="/faq" class="item">
-      FAQ
-    </Link>
-    <Link to="/results" class="item">
-      How It Works
-    </Link>
+      <div class="right menu">
+        <Link to="/carousel" class="item">
+          Press
+        </Link>
+        <Link to="/faq" class="item">
+          FAQ
+        </Link>
+        <Link to="/results" class="item">
+          How It Works
+        </Link>
 
-
-    <div class="item">
-      <div class="ui animated button" tabindex="0">
-        <div class="visible content"> Learn More </div>
-          <div class="hidden content">
-            <i class="right arrow icon"></i>
+        <div class="item">
+          <div class="ui animated button" tabindex="0">
+            <div class="visible content"> Learn More </div>
+            <div class="hidden content">
+              <i class="right arrow icon"></i>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  );
+};
 
-  )
-}
-
-
-export default Navbar
+export default Navbar;
